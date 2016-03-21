dfabb413095c1b022291c2e7767a2101c3b47c4d
Armando Mendoza <fcoarmandomendoza@gmail.com>
2016-03-21 12:47:27 -0430

    Add skills to serializer of experiences

--- app/serializers/academic_experience_serializer.rb
+++ app/serializers/academic_experience_serializer.rb
@@ -1,5 +1,9 @@
 class AcademicExperienceSerializer < ActiveModel::Serializer
   attributes :id, :degree, :institution, :start_date, :end_date,
-    :minimum_grade, :maximum_grade, :actual_grade, :country_code, :city
+    :minimum_grade, :maximum_grade, :actual_grade, :country_code, :city, :skills
   belongs_to :user
+
+  def skills
+    object.skills.pluck(:name)
+  end
 end
--- app/serializers/professional_experience_serializer.rb
+++ app/serializers/professional_experience_serializer.rb
@@ -1,6 +1,10 @@
 class ProfessionalExperienceSerializer < ActiveModel::Serializer
   attributes :id, :position, :organization, :start_date, :end_date,
-    :country_code, :city, :current
+    :country_code, :city, :current, :skills
   belongs_to :user
   belongs_to :sector
+
+  def skills
+    object.skills.pluck(:name)
+  end
 end
